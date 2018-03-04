bef0f8a449eba5151b4f42c323cad8ed488caffb
MXK26 <MXK26@CHE361XNH2.na.lennoxintl.com>
2018-03-04 14:37:33 -0600

    Murali testing

---
+++ Murali/src/main/java/Murali/Murali/App.java
@@ -0,0 +1,13 @@
+package Murali.Murali;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
